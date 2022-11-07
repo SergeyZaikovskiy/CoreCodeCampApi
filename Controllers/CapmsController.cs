64e3dbfb296a802e5cf5877be6d4e6634e7ab839
sega6300 <sega6300@gmail.com>
2022-11-07 16:45:25 +0500

    Add project files.

---
+++ Controllers/CapmsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCampApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class CapmsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCamps()
+        {
+            return Ok(new { Moniker = "ATL305", Name = "MegaCity Code Camp" });
+        }
+    }
+}
