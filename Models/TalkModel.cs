c67e97427a6fbffacde7a38874128c9895f0790b
Sergey Zaykovsky <Sergey.Zaykovsky@Emerson.com>
2022-11-10 09:55:38 +0500

    Add Api methods

---
+++ Models/TalkModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CoreCodeCampApi.Models
+{
+    public class TalkModel
+    {
+        public string Title { get; set; }
+        public string Abstract { get; set; }
+        public int Level { get; set; }
+
+        public SpeakerModel Speakers{ get; set; }
+    }
+}
